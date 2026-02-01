73c488bace8153188735c18d0654bdf4dce1cfdd
Rakib Hoseen <rh01828315879@gmail.com>
2026-02-01 09:33:41 +0600

    add class 4

---
+++ scripting/class_4/usercmd.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#echo "Enter your name : "
+#read name
+#echo $name
+#read -p  "Enter your fullname : " name
+#echo $name
+
+echo "###################################"
+echo "#                                 #"
+echo "#           Sanjida apu           #"
+echo "#                                 #"
+echo "###################################"
+
+echo "1. Enter you ip address"
+echo "2. Enter you mac address"
+read -p "Enter you choice: " option
+echo $option
